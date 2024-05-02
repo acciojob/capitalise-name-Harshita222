7856dd67cbd62c0317fd3e1fc53dd7f86b44293b
Harshita222 <124767756+Harshita222@users.noreply.github.com>
2024-05-02 12:02:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+let inp = document.querySelector("input")
+inp.addEventListener("blur", converted)
+function converted(){
+	let res = inp.value
+		console.log(res.toUpperCase());
+}
